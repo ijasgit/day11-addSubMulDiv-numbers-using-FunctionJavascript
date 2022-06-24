36c7e1bb418dd040edb28e27df400f20fd614df3
Ijasgit <mohammedijasn3899@gmail.com>
2022-06-24 09:29:13 +0530

    addSubmulDiv.

---
+++ add-sub-mul-div.js
@@ -0,0 +1,31 @@
+var num1=100,num2=20;
+
+var answer=add(num1,num2)
+console.log(answer)
+var answer=sub(num1,num2)
+console.log(answer)
+var answer=mul(num1,num2)
+console.log(answer)
+var answer=div(num1,num2)
+console.log(answer)
+function add(x,y)
+{
+    c=x+y;
+    return (c)  
+}
+function sub(x,y)
+{
+    b=x-y;
+    return (b)  
+}
+function mul(x,y)
+{
+    a=x*y;
+    return (a)  
+}
+function div(x,y)
+{
+    d=x/y;
+    return (d)  
+}
+
